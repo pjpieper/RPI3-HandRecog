d58a1d91c33c928781d84f28b23c1366cf4748e1
Paul J. Pieper <55561681+pjpieper@users.noreply.github.com>
2021-05-02 11:45:14 -0400

    Updated handrecog.py. Adding saved model and images.
    Handrecog.py changes:
       Deleted Load_Image function. It no longer served a purpose given the codebase changes.
       Changed how the threshold system works. Went with a recommendation from several sources for OTSU method.
       System now takes keyboard input in order to capture image. 
       Sensehat LED display will now update only when confidence threshold requirements are met.
       run_network function now displays full digit confidence value array.
    
    Model Addition:
       Saved model was added as a .pb. Will update later with a newer model running 15 epoch instead of 9, in which it will be a .h5 format.
    
    Image Addition:
       Saved images from previous test runs showing how filter operates to be added to documentation later.

--- handrecog.py
+++ handrecog.py
@@ -5,96 +5,142 @@
 # Dependencies: OpenCV, Tensorflow, Numpy, Sensehat, Picamera
 # Model: Provided via:  https://keras.io/examples/vision/mnist_convnet/
 #
-# Desc:  Using the control stick on the sensehat (in any direction),
-#        will take a picture using the PiCam. The image will be saved
-#        into the CWD and will be 
+# Desc:  After startup, a live image preview will show in a new window.
+#	 Following this, the system will then take image captures
+#	 using the "C" key, and assessing each as to what digit it
+#	 most resembles, while printing out a confidence value. You can
+#	 also perform a "continuous capture" if you hold down "C". 
+#	 Pressing "Q" will quit the application.
 #######################################################################
 
 from sense_hat import SenseHat # SenseHat 
 from picamera import PiCamera # Camera 
+from skimage import img_as_ubyte
+from skimage.color import rgb2gray
 import time
+from time import sleep
+import datetime
+import argparse
+import imutils
+from imutils.video import VideoStream
 import os, cv2, itertools # cv2 -- OpenCV
-#from Keras.models import load_model # Keras Model Loading
-#from keras.preprocessing.image import img_to_array
 import tensorflow as tf # Tensorflow
 import numpy as np # Numpy
 
 # Assigning SenseHat and PiCamera
 sense = SenseHat()  # Initialize SenseHat
-camera = PiCamera() # Initialize Camera
-camera.color_effects = (128,128) # Set camera to black and white mode
+#camera = PiCamera() # Initialize Camera
+#camera.color_effects = (128,128) # Set camera to black and white mode
 
 #Scaled&Thresh-Pilled 
 ROWS = 28
 COLS = 28
-thresh = 160 #160 for Wht on Blk, 90 for Blk on Wht
 
-# Read Image Function Using OpenCV
-def read_negative_image(file_path):
-	print('Applying filter to image...')
-	#Reads image in using OpenCV grayscale
-	sample_img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-	#Inverts image values
-	sample_img = 255 - sample_img
-	#Binary Filter
-	sample_img = cv2.threshold(sample_img, thresh, 255, cv2.THRESH_BINARY)[1]
-	#Write and Show Filtered Image
-	print('Filter Applied!')
-	cv2.imwrite('samp_BW.png', sample_img)
-	cv2.imshow('Binary Input', sample_img)
-	return cv2.resize(sample_img, (ROWS, COLS),interpolation=cv2.INTER_CUBIC)
+#Load model
+print('Loading Neural Network...\n')
+model = tf.keras.models.load_model('keras_convent_adam')
+#model = tf.keras.models.load_weights('keras_convent_adam')
 
-# load and prepare the image
-def load_image(filename):
-	# load the image
-	file = "sample_img.png"
-	sample_img = read_negative_image(file)
-	# convert to array
-	sample_img = tf.keras.preprocessing.image.img_to_array(sample_img)
-	# reshape into a single sample with 1 channel
-	sample_img = sample_img.reshape(1, ROWS, COLS, 1)
-	# prepare pixel data
-	sample_img = sample_img.astype('float32')
-	sample_img = sample_img / 255.0
-	return sample_img
+# Construct arguments for PiCamera.
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picamera", type=int, default=-1, help="Should the PiCam be used?")
+args = vars(ap.parse_args())
  
-# load an image and predict the class
-def run_network():
-	# load the image
-	sample_img = load_image('sample_img.png')
-	# load model
-	print('Running Neural Network...')
-	model = tf.keras.models.load_model('keras_convnet_adam')
-	# predict the class
-	digit = model.predict_classes(sample_img)
-	predictions = model.predict(sample_img)
-	number = digit[0]
-	print(number)
-	print(predictions[0,number])
-	output =  str(number)
-	sense.show_letter(output)
-	return
+# Initialize VideoStream from PiCamera
+vidStream = VideoStream(usePiCamera=args["picamera"] > 0).start()
+time.sleep(2.0)
 
-# Take a picture after Joystick Input
-def say_cheese():
-        # Capture Image, display
-	print('Taking picture!')
-	camera.capture('sample_img.png')
-	camera.stop_preview()
-	print('Picture Taken!')
-	#cv2.imshow('Number?', sample_img) #, cmap="gray", vmin=0, vmax=255)
-	#time.sleep(2)
-	run_network()
+# Read Image Function Using OpenCV and Create Binary Output
+def read_negative_image(sample_img):
 
-# Main Function - Wait for Joystick to Run Code
-##print('Waiting for joystick input from user...')
-##sense.stick.direction_any = say_cheese
+	print('Applying filter to image...')
 
-while True:
-	print('Taking a picture...')
-	say_cheese()
-	pass
+	#Reads image in using OpenCV grayscale the converts to uint8
+	img_gray =  rgb2gray(sample_img)
+	img_gray8 = img_as_ubyte(img_gray)
 
+	#Binary Filter and Scaling using the OTSU method
+	(thresh, img_bw) = cv2.threshold(img_gray8, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+	img_resized = cv2.resize(img_bw,(28,28))
 
-#while True:
-#	pass  # This keeps the program running to receive joystick events
+	#Invert the colors
+	img_bw_invert = 255 - img_resized
+	img_final = img_bw_invert.reshape(1,ROWS,COLS,1)
+	#Write and Show Filtered Image (For Testing Purposes)
+	#print('Filter Applied!')
+	#cv2.imwrite('samp_BW.png', sample_img)
+	#cv2.imshow('Binary Input', sample_img)
+
+	run_network(img_final)
+
+# Predict the classification of the image.
+def run_network(img_final):
+
+	#Array printout of each possible digit.
+	answer = model.predict(img_final)
+	print(answer)
+	digit = model.predict_classes(img_final)
+
+	#Digit with the greatest possibility is the prediction
+	prediction = answer[0].tolist().index(max(answer[0].tolist()))
+	print('The predicted digit is: ', prediction)
+
+	#Get confidence value
+	confidences = np.squeeze(model.predict(img_final))
+	topClass = np.argmax(confidences)
+	topConf = confidences[topClass]
+	
+	#Update Sensehat LED display when over the 50% threshold
+	if topConf > .5:
+
+		print('Confidence is: ', topConf)
+		ledDigit = str(prediction)
+		sense.show_letter(ledDigit)
+
+##########################################################
+# Main Function - Takes pictures when 'C' is pressed.
+#		  Quits function when 'Q' is pressed.
+#                 Waits for keyboard input to break loop.
+##########################################################
+
+def main():
+	#Updates frames from VideoStream to Window Preview
+	while True:
+		try:
+
+			#Resizes image input from VideoStream to 800px wide, auto adjust height.
+			frame = vidStream.read()
+			frame = imutils.resize(frame, width=800)
+		 
+			#Creates a timestamp on the image.
+			timestamp = datetime.datetime.now()
+			ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+			cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+				0.35, (0, 0, 255), 1)
+		 
+			#Show frame in Window Preview
+			cv2.imshow("Window Preview", frame)
+			key = cv2.waitKey(1) & 0xFF
+		 
+			#Key catch statement. Refer to Main Function comment for controls.
+			if key == ord("q"):
+				break
+				#KillAllWindows.exe
+				cv2.destroyAllWindows()
+				vidStream.stop()
+			elif key == ord("c"):
+				cv2.imwrite("number.jpg", frame)  
+				sample_img = cv2.imread("number.jpg")
+				read_negative_image(sample_img)
+			else:
+				pass
+				
+		except KeyboardInterrupt:
+			#KillAllWindows.exe
+			cv2.destroyAllWindows()
+			vidStream.stop()
+			
+
+if __name__=="__main__":
+	main()
+
